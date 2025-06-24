a686318feae42eed9cf863ff83027cf6c6704677
Jake Massimo <jakemas@amazon.com>
2025-06-24 21:03:57 +0000

    AVX2: Add native implementation of poly_reduce
    Signed-off-by: Jake Massimo <jakemas@amazon.com>

--- mldsa/native/x86_64/src/reduce_avx2.c
+++ mldsa/native/x86_64/src/reduce_avx2.c
@@ -20,34 +20,6 @@
 #include "consts.h"
 
 /*************************************************
- * Name:        mld_poly_reduce_avx2
- *
- * Description: Inplace reduction of all coefficients of polynomial to
- *              representative in [-6283009,6283008]. Assumes input
- *              coefficients to be at most 2^31 - 2^22 - 1 in absolute value.
- *
- * Arguments:   - int32_t *r: pointer to input/output polynomial
- **************************************************/
-void mld_poly_reduce_avx2(int32_t *r)
-{
-  unsigned int i;
-  __m256i f, g;
-  const __m256i q = _mm256_set1_epi32(MLDSA_Q);
-  const __m256i off = _mm256_set1_epi32(1 << 22);
-  __m256i *rr = (__m256i *)r;
-
-  for (i = 0; i < MLDSA_N / 8; i++)
-  {
-    f = _mm256_load_si256(&rr[i]);
-    g = _mm256_add_epi32(f, off);
-    g = _mm256_srai_epi32(g, 23);
-    g = _mm256_mullo_epi32(g, q);
-    f = _mm256_sub_epi32(f, g);
-    _mm256_store_si256(&rr[i], f);
-  }
-}
-
-/*************************************************
  * Name:        mld_poly_caddq_avx2
  *
  * Description: For all coefficients of in/out polynomial add Q if
