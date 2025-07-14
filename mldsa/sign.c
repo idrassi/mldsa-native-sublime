e2b54c7ddb7f171da06888fcbd8fde85f297107e
Matthias J. Kannwischer <matthias@kannwischer.eu>
2025-07-14 14:24:58 +0800

    Namespacing: type polyvecl -> mld_polyvecl
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>

--- mldsa/sign.c
+++ mldsa/sign.c
@@ -14,10 +14,10 @@
 #include "sign.h"
 #include "symmetric.h"
 
-static void mld_sample_s1_s2(polyvecl *s1, polyveck *s2,
+static void mld_sample_s1_s2(mld_polyvecl *s1, polyveck *s2,
                              const uint8_t seed[MLDSA_CRHBYTES])
 __contract__(
-  requires(memory_no_alias(s1, sizeof(polyvecl)))
+  requires(memory_no_alias(s1, sizeof(mld_polyvecl)))
   requires(memory_no_alias(s2, sizeof(polyveck)))
   requires(memory_no_alias(seed, MLDSA_CRHBYTES))
   assigns(object_whole(s1), object_whole(s2))
@@ -59,8 +59,8 @@ int crypto_sign_keypair_internal(uint8_t *pk, uint8_t *sk,
   uint8_t inbuf[MLDSA_SEEDBYTES + 2];
   uint8_t tr[MLDSA_TRBYTES];
   const uint8_t *rho, *rhoprime, *key;
-  polyvecl mat[MLDSA_K];
-  polyvecl s1, s1hat;
+  mld_polyvecl mat[MLDSA_K];
+  mld_polyvecl s1, s1hat;
   polyveck s2, t2, t1, t0;
 
   /* Get randomness for rho, rhoprime and key */
@@ -443,7 +443,7 @@ int crypto_sign_verify_internal(const uint8_t *sig, size_t siglen,
   uint8_t c[MLDSA_CTILDEBYTES];
   uint8_t c2[MLDSA_CTILDEBYTES];
   mld_poly cp;
-  polyvecl mat[MLDSA_K], z;
+  mld_polyvecl mat[MLDSA_K], z;
   polyveck t1, w1, tmp, h;
 
   if (siglen != CRYPTO_BYTES)
