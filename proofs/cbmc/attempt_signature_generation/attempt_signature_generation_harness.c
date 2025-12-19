9046e5a4f4ab3f1f18224cb39d926cc402ed523f
Matthias J. Kannwischer <matthias@kannwischer.eu>
2025-12-19 10:10:20 +0800

    CBMC: trim down to track down union regression
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>

--- proofs/cbmc/attempt_signature_generation/attempt_signature_generation_harness.c
+++ proofs/cbmc/attempt_signature_generation/attempt_signature_generation_harness.c
@@ -3,25 +3,19 @@
 
 #include "sign.h"
 
-int mld_attempt_signature_generation(uint8_t *sig, const uint8_t *mu,
-                                     const uint8_t rhoprime[MLDSA_CRHBYTES],
-                                     uint16_t nonce, mld_polymat *mat,
-                                     const mld_polyvecl *s1,
+int mld_attempt_signature_generation(const uint8_t rhoprime[MLDSA_CRHBYTES],
+                                     uint16_t nonce,
                                      const mld_polyveck *s2,
-                                     const mld_polyveck *t0);
+                                     mld_poly *cp);
 
 void harness(void)
 {
-  uint8_t *sig;
-  uint8_t *mu;
   uint8_t *rhoprime;
   uint16_t nonce;
   mld_polymat *mat;
-  mld_polyvecl *s1;
   mld_polyveck *s2;
-  mld_polyveck *t0;
+  mld_poly *cp;
 
   int r;
-  r = mld_attempt_signature_generation(sig, mu, rhoprime, nonce, mat, s1, s2,
-                                       t0);
+  r = mld_attempt_signature_generation(rhoprime, nonce, s2, cp);
 }
